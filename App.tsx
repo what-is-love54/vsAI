9696b18e256323c6a295e021ec4aa6d0f56e4e33
Vlad <vlidik1345@gmail.com>
2025-11-26 19:08:43 +0200

    add text from windows

--- App.tsx
+++ App.tsx
@@ -1,7 +1,7 @@
 /** @format */
 
 import {NewAppScreen} from '@react-native/new-app-screen';
-import {StatusBar, StyleSheet, useColorScheme, View} from 'react-native';
+import {StatusBar, StyleSheet, useColorScheme, View, Text} from 'react-native';
 import {
 	SafeAreaProvider,
 	useSafeAreaInsets,
@@ -30,6 +30,9 @@ function AppContent() {
 				templateFileName="App.tsx"
 				safeAreaInsets={safeAreaInsets}
 			/>
+			<View>
+				<Text>Hello world</Text>
+			</View>
 		</View>
 	);
 }
